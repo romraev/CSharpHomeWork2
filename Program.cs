ac0518b42f032847dfee44c3fc40370d44e6ce18
Romraev <romraev@yandex.ru>
2023-03-14 17:53:24 +0300

    third task

--- Program.cs
+++ Program.cs
@@ -24,27 +24,41 @@
 
 // Task 13.
 
-int ThirdDigit(int num)
-{
-    if (num < 100)
-    {
-        int result = -1;
-        return result;
-    }
-    else
-    {
-        while (num > 999)
-        {
-             num = num / 10;
-        }
-        int result = num % 10;
-        return result;
-    }
+// int ThirdDigit(int num)
+// {
+//     if (num < 100)
+//     {
+//         int result = -1;
+//         return result;
+//     }
+//     else
+//     {
+//         while (num > 999)
+//         {
+//              num = num / 10;
+//         }
+//         int result = num % 10;
+//         return result;
+//     }
 
+// }
+
+// Console.Write("Hello, friend. Enter your number: ");
+// int Number = Convert.ToInt32(Console.ReadLine());
+
+// if (ThirdDigit(Number) < 0) Console.WriteLine("Your number has less than 3 digits");
+// else Console.WriteLine("The third digit is: " + ThirdDigit(Number));
+
+// Task 15
+
+bool Wknd(int num)
+{
+    if (num == 6 || num == 7) return true;
+    else return false;
 }
 
-Console.Write("Hello, friend. Enter your number: ");
+Console.Write("Hello, friend. Enter your day's number: ");
 int Number = Convert.ToInt32(Console.ReadLine());
 
-if (ThirdDigit(Number) < 0) Console.WriteLine("Your number has less than 3 digits");
-else Console.WriteLine("The third digit is: " + ThirdDigit(Number));
+if (Number > 7) Console.WriteLine("There is no such day");
+else Console.WriteLine("It's time to have a drink! - " + Wknd(Number) + " statement");
