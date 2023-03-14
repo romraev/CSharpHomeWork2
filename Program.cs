9e718bb00149a0478cfd06420925f402985fbf96
Romraev <romraev@yandex.ru>
2023-03-14 17:12:49 +0300

    first task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Подготовил Раевский Р.Ю.
+// Task 10.
+
+int MidNumber(int num)
+{
+    if (num < 100 || num > 999)
+    {
+        int result = -1;
+        return result;
+    }
+    else
+    {
+        int result = (num / 10) % 10;
+        return result;
+    }
+
+}
+
+Console.Write("Hello, friend. Enter your number: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+if (MidNumber(Number) < 0) Console.WriteLine("Your number has less than 3 digits");
+else Console.WriteLine("The middle number is: " + MidNumber(Number));
